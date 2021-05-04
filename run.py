4a58ac9e98fa655c535a65a56848c0cf669414de
kacper2280 <72551434+kacper2280@users.noreply.github.com>
2021-05-04 10:57:22 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -2,30 +2,29 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import abort, redirect, url_for, make_response
-
 app = Flask(__name__)
 
+#add pages here
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.route('/help')
-def help():
-    return 'help me'
+@app.route('/about')
+def about():
+    return render_template('aboutme.html')
 
 @app.route('/contact')
-def contect():
-    return 'contact with me'
+def contact():
+    return render_template('contact.html')
 
-@app.route('/user/<username>', methods=['GET', 'POST'])
-def show_user_profile(username):
-    if request.method == 'POST':
-        return 'HTTP POST for user %s with password %s' % (username, request.form['password'])
-    else:
-        return 'HTTP GET for user %s' % username
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+#Errors
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
 
 @app.route('/error_denied')
 def error_denied():
@@ -41,16 +40,5 @@ def error_not_found():
     response.headers['X-Something'] = 'A value'
     return response
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return request.form['username'] + " + " + request.form['password']
-    else:    
-        return render_template('login.html')
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
